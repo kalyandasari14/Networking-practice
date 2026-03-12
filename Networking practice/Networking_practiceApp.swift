7fdaf375e7f610597bd58e97fb0436ddc0cd4d03
kalayn <kalyan>
2026-03-12 16:26:46 -0500

    Initial Commit

---
+++ Networking practice/Networking_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Networking_practiceApp.swift
+//  Networking practice
+//
+//  Created by kalyan on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct Networking_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
